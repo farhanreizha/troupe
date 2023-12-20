44ad1d15b68e9e6caeb8f597c9c5625cef032137
farhanreziha <reizha226@gmail.com>
2023-12-20 20:56:41 +0700

    setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,20 @@
-import { GeistSans } from 'geist/font/sans'
-import './globals.css'
+import { Open_Sans } from "next/font/google";
+import "./globals.css";
 
-const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : 'http://localhost:3000'
+const defaultUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000";
+
+const font = Open_Sans({ subsets: ["latin"] });
 
 export const metadata = {
-  metadataBase: new URL(defaultUrl),
-  title: 'Next.js and Supabase Starter Kit',
-  description: 'The fastest way to build apps with Next.js and Supabase',
-}
+	metadataBase: new URL(defaultUrl),
+	title: "troupe",
+	description: "troupe is an application for socializing and playing any game with new and old friends",
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en" className={GeistSans.className}>
-      <body className="bg-background text-foreground">
-        <main className="min-h-screen flex flex-col items-center">
-          {children}
-        </main>
-      </body>
-    </html>
-  )
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+	return (
+		<html lang="en">
+			<body className={font.className}>{children}</body>
+		</html>
+	);
 }
