99b324f693b5176b4c5085a78d6882e2f0343107
farhanreziha <reizha226@gmail.com>
2024-01-07 03:30:43 +0700

    deleted query

--- lib/query/update.ts
+++
@@ -1,8 +0,0 @@
-import { db } from "@lib/db";
-
-export async function updateProfile({ profileId, displayName, imageUrl }: { profileId: string; displayName: string; imageUrl: string }) {
-  return await db.profile.update({
-    where: { id: profileId },
-    data: { displayName, imageUrl },
-  });
-}
